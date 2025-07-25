8fe4f55eec2d01143eea50e8e722b2219177c8e5
Zarif Atai <z.atai@protonmail.com>
2025-07-25 23:25:30 +0200

    Changed float window size

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,7 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = { float = { padding = 5 } },
+  opts = { float = { max_width = 80, max_height = 30, } },
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
