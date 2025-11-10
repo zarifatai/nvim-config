52f922d1941e31bf646e2817629321fe48e4f870
Zarif Atai <z.atai@protonmail.com>
2025-11-10 23:46:14 +0100

    Enabled signs in todo-comments plugin

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,1 +1,1 @@
-return { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } }
+return { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = {} }
