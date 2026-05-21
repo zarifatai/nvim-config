cc33490e46cf7e8b93572b4f58e84494cb72b9f7
Zarif Atai <z.atai@protonmail.com>
2026-05-21 11:14:21 +0200

    Fix bugs and extend conform config

--- plugin/conform.lua
+++ plugin/conform.lua
@@ -1,44 +1,34 @@
-vim.api.nvim_create_autocmd('BufWritePre', {
-  once = true,
-  callback = function()
-    vim.pack.add({
-      'https://github.com/stevearc/conform.nvim',
-    })
-    require('conform').setup({
-      cmd = { 'ConformInfo' },
-      keys = {
-        {
-          '<leader>f',
-          function()
-            require('conform').format { async = true, lsp_format = 'fallback' }
-          end,
-          mode = '',
-          desc = '[F]ormat buffer',
-        },
-      },
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
-      },
-    })
-  end,
+vim.pack.add({ "https://github.com/stevearc/conform.nvim" })
+require("conform").setup({
+	notify_on_error = false,
+	format_on_save = function(bufnr)
+		-- Disable "format_on_save lsp_fallback" for languages that don't
+		-- have a well standardized coding style. You can add additional
+		-- languages here or re-enable it for the disabled ones.
+		local disable_filetypes = { c = true, cpp = true }
+		if disable_filetypes[vim.bo[bufnr].filetype] then
+			return nil
+		else
+			return {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			}
+		end
+	end,
+	formatters_by_ft = {
+		lua = { "stylua" },
+		rust = { "rustfmt", lsp_format = "fallback" },
+		html = { "oxfmt" },
+		javascript = { "oxfmt" },
+		typescript = { "oxfmt" },
+		svelte = { "biome" },
+		css = { "oxfmt" },
+		markdown = { "oxfmt" },
+		json = { "oxfmt" },
+		-- Conform can also run multiple formatters sequentially
+		-- python = { "isort", "black" },
+		--
+		-- You can use 'stop_after_first' to run the first available formatter from the list
+		-- javascript = { "prettierd", "prettier", stop_after_first = true },
+	},
 })
